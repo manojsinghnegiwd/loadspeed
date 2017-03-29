f1adbe699822162babc4c81cb48cf132d0328d55
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-30 01:21:56 +0530

    added urlslist

--- app/frontEnd/App.js
+++ app/frontEnd/App.js
@@ -7,11 +7,19 @@ export default class App extends Component {
     this.socket = client('http://localhost:8080');
     this.socket.on('connect', () => console.log('connected'));
     this.socket.on('end_page_load', (data) => console.log(data));
+    this.socket.on('new_website_tested', (data) => this.updateList(data));
 
     this.state = {
-      url: ''
+      url: '',
+      urlsList: []
     }
 
+  }
+
+  updateList = (data) => {
+    this.setState((prevState) => ({
+      urlsList: prevState.urlsList.concat([data])
+    }))
   }
 
   loadPage = (url) => {
@@ -30,6 +38,7 @@ export default class App extends Component {
       <div>
         <input onChange={this.updateUrl} value={url} />
         <button onClick={() => this.loadPage(url)}>Start Loading</button>
+        {urlsList && urlsList.map((url, index) => <li key={index}>{url.address} : {url.load_time}</li>)}
       </div>
     )
   }
