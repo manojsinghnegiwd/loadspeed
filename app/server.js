8f8223bdb9d32e51ca7b5ac82c0f1a1b187c6ab0
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-30 01:01:46 +0530

    created loadpage function

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 import http from 'http';
 import socketIo from 'socket.io';
 import fs from 'fs';
+import request from 'request';
 
 // handle routes
 const handler = (req, res) => {
@@ -11,6 +12,24 @@ const handler = (req, res) => {
 
 const app = http.createServer(handler);
 const io = socketIo(app);
+
+const loadPage = (url) => {
+  return new Promise((res, rej) => {
+    let time = Date.now();
+    request(url, (err, response) => {
+      time = Date.now() - time;
+
+      if(err)
+        return rej(err);
+
+      return res({
+        'statusCode:', response && response.statusCode,
+        'load_time': time
+      })
+
+    })
+  })
+}
 
 io.on('connection', (socket) => {
   console.log(socket.id);
