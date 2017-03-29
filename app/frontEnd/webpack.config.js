70b268cd500c08be3135827370c23ed8773e150d
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-30 00:17:05 +0530

    added webpack config

---
+++ app/frontEnd/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: [
+    './index.jsx'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/static',
+    publicPath: '/static/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+    historyApiFallback: true
+  }
