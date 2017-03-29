3c49d19ef3793418693bbcc32064b83f50d13155
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-30 02:09:51 +0530

    implemented loading

--- app/frontEnd/App.js
+++ app/frontEnd/App.js
@@ -6,24 +6,25 @@ export default class App extends Component {
     super(props);
     this.socket = client('http://localhost:7000');
     this.socket.on('connect', () => console.log('connected'));
-    this.socket.on('end_page_load', (data) => console.log(data));
+    this.socket.on('end_page_load', (data) => this.updateCalculating(false));
     this.socket.on('new_website_tested', (data) => this.updateList(data));
 
     this.state = {
       url: '',
-      urlsList: []
+      urlsList: [],
+      calculating: false
     }
 
   }
 
   updateList = (data) => {
-    console.log(data);
     this.setState((prevState) => ({
       urlsList: prevState.urlsList.concat([data])
     }))
   }
 
   loadPage = (url) => {
+    this.updateCalculating(true);
     this.socket.emit('start_page_load', {url});
   }
 
@@ -33,12 +34,19 @@ export default class App extends Component {
     })
   }
 
+  updateCalculating = (flag) => {
+    this.setState({
+      calculating: flag
+    })
+  }
+
   render () {
-    const {url, urlsList} = this.state;
+    const {url, urlsList, calculating} = this.state;
     return (
       <div>
         <input onChange={this.updateUrl} value={url} />
         <button onClick={() => this.loadPage(url)}>Start Loading</button>
+        {calculating ? <p>loading</p> : null}
         {urlsList && urlsList.map((url, index) => <li key={index}>{url.address} : {url.load_time}</li>)}
       </div>
     )
