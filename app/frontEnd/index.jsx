13b753be555b962bd14b37280d3c77309280b3fc
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-31 00:33:18 +0530

    imorted elemental less

--- app/frontEnd/index.jsx
+++ app/frontEnd/index.jsx
@@ -2,4 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.js';
 
+import 'elemental/less/elemental.less';
+
 ReactDOM.render(<App />, document.getElementById('app'));
