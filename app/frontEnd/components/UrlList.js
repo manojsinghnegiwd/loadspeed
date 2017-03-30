6ffbb59967a8410e12ea77c020e72ff5cdf26e1e
Manoj Singh Negi <justanothermanoj@gmail.com>
2017-03-31 00:42:47 +0530

    added UrlList component

---
+++ app/frontEnd/components/UrlList.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export default class UrlList extends Component {
+  render () {
+    const urls = this.props;
+    console.log(urls);
+    return (
+      <div></div>
+    )
+  }
+}
